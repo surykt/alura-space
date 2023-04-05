4b4b6a572096f634f634f1e0956b979e9c02f355
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-04-05 12:26:28 -0300

    add banner component

--- src/pages/InitialPage/index.jsx
+++ src/pages/InitialPage/index.jsx
@@ -1,5 +1,21 @@
 import Header from "../../components/Header";
+import Menu from "../../components/Menu";
+import banner from "./banner.png";
+import styles from "./InitialPage.module.scss";
 
 export default function InitialPage() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__image}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
