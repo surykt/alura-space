b5bd5d51df93192b6abbd641abc92d7321f33d45
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-04-06 11:40:14 -0300

    create tags component

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Tags from "../Tags";
+
 export default function Gallery() {
   return (
     <section>
       <h2>Navegue pela galeria</h2>
+      <Tags />
       <ul></ul>
     </section>
   );
