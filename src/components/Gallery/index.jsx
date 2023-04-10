d5638c5ee1a6b1beef282d2dec6578bf18ee7f98
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-04-10 18:05:42 -0300

    create state to cards component

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Tags from "../Tags";
 import fotos from "./fotos.json";
 import styles from "./Gallery.module.scss";
 import Cards from "./Cards";
 
 export default function Gallery() {
+  const [items, setItems] = useState(fotos);
+
   return (
     <section className={styles.gallery}>
       <h2>Navegue pela galeria</h2>
       <Tags />
-      <Cards items={fotos} styles={styles} />
+      <Cards items={items} styles={styles} />
     </section>
   );
 }
